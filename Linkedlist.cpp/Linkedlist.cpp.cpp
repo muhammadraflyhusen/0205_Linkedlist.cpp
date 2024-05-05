7ef91284369a2df7f1021c4b818ae8ae03aeb6a6
muhammadraflyhusen <raflyhusen311@gmail.com>
2024-05-05 10:04:14 +0700

    start from the first node & previous node is NULL initially

--- Linkedlist.cpp/Linkedlist.cpp.cpp
+++ Linkedlist.cpp/Linkedlist.cpp.cpp
@@ -40,3 +40,6 @@ void addnode()
     }
     else
     {
+        // insert the new node in the middle or at the end 
+        Node* current = START; // step 1.a : start from the first node 
+        Node* previous = NULL; // step 1.b : previous node is NULL initially
