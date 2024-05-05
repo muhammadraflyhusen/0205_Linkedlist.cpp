4a841c66d044f81e7673da529e21a7ca5a069d55
muhammadraflyhusen <raflyhusen311@gmail.com>
2024-05-05 10:06:19 +0700

    traverse the list to find the correct posiition,move the previous to the current node&move the current to the next node

--- Linkedlist.cpp/Linkedlist.cpp.cpp
+++ Linkedlist.cpp/Linkedlist.cpp.cpp
@@ -43,3 +43,8 @@ void addnode()
         // insert the new node in the middle or at the end 
         Node* current = START; // step 1.a : start from the first node 
         Node* previous = NULL; // step 1.b : previous node is NULL initially
+        while (current != NULL && current->noMhs < newNode->noMhs)
+        {                               // step 1.c : traverse  the list to find the correct posiition 
+            previous = current;         // step 1.d : move the previous to the current node
+            current = current->next;    // step 1.e : move the current to the next node
+        }
