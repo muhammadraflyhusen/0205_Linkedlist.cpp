9827d4b17ccf9ce63c6b3564cf1024ca0f174832
muhammadraflyhusen <raflyhusen311@gmail.com>
2024-05-05 09:58:18 +0700

    insert the new node in the list

--- Linkedlist.cpp/Linkedlist.cpp.cpp
+++ Linkedlist.cpp/Linkedlist.cpp.cpp
@@ -20,3 +20,11 @@ void addnode()
     cout << "\nEnter the name of the student: ";
     cin >> newNode->name; // assign value to the data field of the new node 
 
+    // Insert the new node in the list 
+    if (START == NULL || newNode->noMhs <= START->noMhs)
+    { // step2 : insert the new Node at the beginning
+
+        if (START != NULL && newNode->noMhs == START->noMhs)
+        {
+            cout << "\033[31mDuplicate roll numbers not allowed\033[0m" << endl;
+            return;
